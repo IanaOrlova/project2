0ddca90c191559a3b27ed49563da6405f8b36bb6
Yana <38989612+IanaOrlova@users.noreply.github.com>
2018-10-01 23:57:42 +0300

    bootstrap

---
+++ script/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+
+
+    // $('.slider').slick({
+    //     infinite: true,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     arrows: false,
+    //     // dots: true,
+    //     autoplay: true,
+    //     // prevArrow: '<img class="prev" src="img/left.png">',
+    //     // nextArrow: '<img class="next" src="img/right.png">',
+    //
+    //
+    // });
+
+
+})
+$('.carousel').carousel({
+    interval: 2000
+
+})
