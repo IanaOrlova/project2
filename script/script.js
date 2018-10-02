bfcb44934efc6926ed947a17d904ffc237cffdf2
Yana <38989612+IanaOrlova@users.noreply.github.com>
2018-10-03 00:06:19 +0300

    responsive

--- script/script.js
+++ script/script.js
@@ -1,23 +1,7 @@
 $(document).ready(function () {
 
+    $('.carousel').carousel({
+        interval: 3000
 
-
-    // $('.slider').slick({
-    //     infinite: true,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     arrows: false,
-    //     // dots: true,
-    //     autoplay: true,
-    //     // prevArrow: '<img class="prev" src="img/left.png">',
-    //     // nextArrow: '<img class="next" src="img/right.png">',
-    //
-    //
-    // });
-
-
-})
-$('.carousel').carousel({
-    interval: 2000
-
+    })
 })
